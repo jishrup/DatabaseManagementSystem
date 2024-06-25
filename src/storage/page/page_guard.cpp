6d206b7b671d8db47099c07c6a8fd6fa916d4ea5
jishrup <jishnusm1997@gmail.com>
2024-06-24 20:07:12 -0700

    Project2 : Implement Page Guards for Buffer Pool Management
    Task #1 - Read/Write Page Guards
    
    - Implemented BasicPageGuard to manage page unpinning automatically upon going out of scope.
    - Added methods for manual unpinning and APIs for read-only/write data access with compile-time checks for the is_dirty flag.
    - Implemented ReadPageGuard and WritePageGuard to handle reader-writer latching, automatically unlatching pages when guards go out of scope.
    - Added move constructor, move assignment operator, Drop method, and destructor for BasicPageGuard, ReadPageGuard, and WritePageGuard.
    - Implemented UpgradeRead and UpgradeWrite functions in BasicPageGuard to ensure pages are not evicted during upgrades.
    - Added wrappers in BufferPoolManager: FetchPageBasic, FetchPageRead, FetchPageWrite, and NewPageGuarded.
    
    These changes ensure that pages are properly managed and unpinned/latches are released automatically, reducing the risk of programmer error and improving buffer pool performance.

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -3,36 +3,217 @@
 
 namespace bustub {
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
+  // copy the contents(ownership) of the that pointer to this pointer
+  this->bpm_ = that.bpm_;
+  this->page_ = that.page_;
+  this->is_dirty_ = that.is_dirty_;
 
-void BasicPageGuard::Drop() {}
+  // make the contents of that pointer to null
+  that.page_ = nullptr;
+  that.is_dirty_ = false;
+  that.bpm_ = nullptr;
+}
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { return *this; }
+void BasicPageGuard::Drop() {
+  if(bpm_ != nullptr && page_ != nullptr) {
+    // unpin this page
+    bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
 
-BasicPageGuard::~BasicPageGuard(){};  // NOLINT
+    // make the contents of that pointer to null
+    page_ = nullptr;
+    is_dirty_ = false;
+    bpm_ = nullptr;
+  }
+}
 
-auto BasicPageGuard::UpgradeRead() -> ReadPageGuard { return {bpm_, page_}; }
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { 
+  if(this != &that) {
+    //Drop the existing page
+    Drop();
 
-auto BasicPageGuard::UpgradeWrite() -> WritePageGuard { return {bpm_, page_}; }
+    // copy the contents(ownership) of the that pointer to this pointer
+    this->bpm_ = that.bpm_;
+    this->page_ = that.page_;
+    this->is_dirty_ = that.is_dirty_;
 
-ReadPageGuard::ReadPageGuard(BufferPoolManager *bpm, Page *page) {}
+    // make the contents of that pointer to null
+    that.bpm_ = nullptr;
+    that.page_ = nullptr;
+    that.is_dirty_ = false;
+  }
 
-ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
+  return *this; 
+}
 
-auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & { return *this; }
+BasicPageGuard::~BasicPageGuard(){
+  if(bpm_ != nullptr && page_ != nullptr) {
+    // unpin this page
+    bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
 
-void ReadPageGuard::Drop() {}
+    // make the contents of that pointer to null
+    page_ = nullptr;
+    is_dirty_ = false;
+    bpm_ = nullptr;
+  }
+};  // NOLINT
 
-ReadPageGuard::~ReadPageGuard() {}  // NOLINT
+auto BasicPageGuard::UpgradeRead() -> ReadPageGuard { 
+  // make an instance of ReadPageGuard by passing the correct parameters
+  auto guarded_read_page = ReadPageGuard(bpm_, page_);
+  
+  // make the contents of that pointer to null
+  page_ = nullptr;
+  is_dirty_ = false;
+  bpm_ = nullptr;
 
-WritePageGuard::WritePageGuard(BufferPoolManager *bpm, Page *page) {}
+  return guarded_read_page; 
+}
 
-WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept = default;
+auto BasicPageGuard::UpgradeWrite() -> WritePageGuard { 
+  // make an instance of WritePageGuard by passing the correct parameters
+  auto guarded_write_page = WritePageGuard(bpm_, page_);
+  
+  // make the contents of that pointer to null
+  page_ = nullptr;
+  is_dirty_ = false;
+  bpm_ = nullptr;
+  
+  return guarded_write_page; 
+}
 
-auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & { return *this; }
+ReadPageGuard::ReadPageGuard(BufferPoolManager *bpm, Page *page) {
+  // Get the read latch for this page
+  page->RLatch();
 
-void WritePageGuard::Drop() {}
+  // copy the contents(ownership) of the that pointer to this pointer
+  guard_.bpm_ = bpm;
+  guard_.page_ = page;
+  guard_.is_dirty_ = page->IsDirty();
+}
 
-WritePageGuard::~WritePageGuard() {}  // NOLINT
+ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept {
+  // Move the latches from that pointer to this pointer
+  guard_ = std::move(that.guard_);
+
+  // copy the contents(ownership) of the that pointer to this pointer
+  this->guard_.bpm_ = that.guard_.bpm_;
+  this->guard_.page_ = that.guard_.page_;
+  this->guard_.is_dirty_ = that.guard_.is_dirty_;
+
+  // make the contents of that pointer to null
+  that.guard_.page_ = nullptr;
+  that.guard_.is_dirty_ = false;
+  that.guard_.bpm_ = nullptr;
+}
+
+auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & { 
+  if(this != &that) {
+    //Drop the existing page
+    Drop();
+
+    // Move the latches from that pointer to this pointer
+    guard_ = std::move(that.guard_);
+
+    // copy the contents(ownership) of the that pointer to this pointer
+    this->guard_.bpm_ = that.guard_.bpm_;
+    this->guard_.page_ = that.guard_.page_;
+    this->guard_.is_dirty_ = that.guard_.is_dirty_;
+
+    // make the contents of that pointer to null
+    that.guard_.bpm_ = nullptr;
+    that.guard_.page_ = nullptr;
+    that.guard_.is_dirty_ = false;
+  }
+
+  return *this;   
+}
+
+void ReadPageGuard::Drop() {
+  if(guard_.bpm_ != nullptr && guard_.page_ != nullptr) {
+    // Release the Read latch
+    guard_.page_->RUnlatch();
+
+    // Drop this page
+    guard_.Drop();
+  }
+}
+
+ReadPageGuard::~ReadPageGuard() {
+  if(guard_.bpm_ != nullptr && guard_.page_ != nullptr) {
+    // Release the Read latch
+    guard_.page_->RUnlatch();
+
+    // Drop this page
+    guard_.Drop();
+  }
+}  // NOLINT
+
+WritePageGuard::WritePageGuard(BufferPoolManager *bpm, Page *page) {
+  // Get the read latch for this page
+  page->WLatch();
+
+  // copy the contents(ownership) of the that pointer to this pointer
+  guard_.bpm_ = bpm;
+  guard_.page_ = page;
+  guard_.is_dirty_ = page->IsDirty();
+}
+
+WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept {
+  // Move the latches from that pointer to this pointer
+  guard_ = std::move(that.guard_);
+
+  // copy the contents(ownership) of the that pointer to this pointer
+  this->guard_.bpm_ = that.guard_.bpm_;
+  this->guard_.page_ = that.guard_.page_;
+  this->guard_.is_dirty_ = that.guard_.is_dirty_;
+
+  // make the contents of that pointer to null
+  that.guard_.page_ = nullptr;
+  that.guard_.is_dirty_ = false;
+  that.guard_.bpm_ = nullptr;
+}
+
+auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & { 
+  if(this != &that) {
+    //Drop the existing page
+    Drop();
+
+    // Move the latches from that pointer to this pointer
+    guard_ = std::move(that.guard_);
+
+    // copy the contents(ownership) of the that pointer to this pointer
+    this->guard_.bpm_ = that.guard_.bpm_;
+    this->guard_.page_ = that.guard_.page_;
+    this->guard_.is_dirty_ = that.guard_.is_dirty_;
+
+    // make the contents of that pointer to null
+    that.guard_.bpm_ = nullptr;
+    that.guard_.page_ = nullptr;
+    that.guard_.is_dirty_ = false;
+  }
+
+  return *this;
+ }
+
+void WritePageGuard::Drop() {
+  if(guard_.bpm_ != nullptr && guard_.page_ != nullptr) {
+    // Release the Read latch
+    guard_.page_->WUnlatch();
+
+    // Drop this page
+    guard_.Drop();
+  }
+}
+
+WritePageGuard::~WritePageGuard() {
+  if(guard_.bpm_ != nullptr && guard_.page_ != nullptr) {
+    // Release the Read latch
+    guard_.page_->WUnlatch();
+
+    // Drop this page
+    guard_.Drop();
+  }
+}  // NOLINT
 
 }  // namespace bustub
