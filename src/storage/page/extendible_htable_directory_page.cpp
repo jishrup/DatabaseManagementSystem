4b18cb7ad2822c63d792c021e1096ebbfccc5a9d
jishrup <jishnusm1997@gmail.com>
2024-06-26 09:51:09 -0700

    Project 2 : Add Hash Table Directory Page Implementation
    Directory pages sit at the second level of our disk-based extendible hash table. Each of them stores the logical child pointers to the bucket pages (as page ids), as well as metadata for handling bucket mapping and dynamic directory growing and shrinking. The directory page has the following fields:
    
    - `max_depth_` : The maximum depth the header page could handle.
    - `global_depth_` : The current directory global depth.
    - `local_depths_` : An array of bucket page local depths.
    - `bucket_page_ids_` : An array of bucket page ids.
    
    Note that although there is a physical limit on how large a page is, you should use `max_depth_` to determine the upper bound of your `bucket_page_ids_` array size.
    
    The extendible hash table directory page is implemented by modifying its header file (`src/include/storage/page/extendible_htable_directory_page.h`) and corresponding source file (`src/storage/page/extendible_htable_directory_page.cpp`).
    
    Key changes include:
    - Initialization of directory page metadata and arrays.
    - Implementation of methods for accessing and modifying directory page ids, local depths, and calculating split images.
    - Utility methods for managing global and local depths, verifying integrity, and printing the directory.
    
    These changes ensure the directory page can dynamically grow and shrink, effectively managing the bucket mappings for the extendible hash table.

--- src/storage/page/extendible_htable_directory_page.cpp
+++ src/storage/page/extendible_htable_directory_page.cpp
@@ -21,45 +21,111 @@
 namespace bustub {
 
 void ExtendibleHTableDirectoryPage::Init(uint32_t max_depth) {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  BUSTUB_ASSERT(max_depth > HTABLE_DIRECTORY_MAX_DEPTH, "Invalid max_depth");
+
+  max_depth_ = max_depth;
+  global_depth_ = 0;
+
+  for(int i = 0; i < HTABLE_DIRECTORY_ARRAY_SIZE; i++) {
+    local_depths_[i] = 0;
+    bucket_page_ids_[i] = INVALID_PAGE_ID;
+  }
 }
 
-auto ExtendibleHTableDirectoryPage::HashToBucketIndex(uint32_t hash) const -> uint32_t { return 0; }
+auto ExtendibleHTableDirectoryPage::HashToBucketIndex(uint32_t hash) const -> uint32_t { 
+  return hash & ((1 << global_depth_) - 1);
+}
 
-auto ExtendibleHTableDirectoryPage::GetBucketPageId(uint32_t bucket_idx) const -> page_id_t { return INVALID_PAGE_ID; }
+auto ExtendibleHTableDirectoryPage::GetBucketPageId(uint32_t bucket_idx) const -> page_id_t { 
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+
+  return bucket_page_ids_[bucket_idx];
+}
 
 void ExtendibleHTableDirectoryPage::SetBucketPageId(uint32_t bucket_idx, page_id_t bucket_page_id) {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+
+  bucket_page_ids_[bucket_idx] =  bucket_page_id;
 }
 
-auto ExtendibleHTableDirectoryPage::GetSplitImageIndex(uint32_t bucket_idx) const -> uint32_t { return 0; }
+auto ExtendibleHTableDirectoryPage::GetSplitImageIndex(uint32_t bucket_idx) const -> uint32_t {
+  if(global_depth_ == 0) {
+    return 0; 
+  }
 
-auto ExtendibleHTableDirectoryPage::GetGlobalDepth() const -> uint32_t { return 0; }
+  return bucket_idx ^ (1 << (global_depth_ - 1));
+}
+
+auto ExtendibleHTableDirectoryPage::GetGlobalDepthMask() const -> uint32_t {
+  return (1 << global_depth_) - 1;
+}
+
+auto ExtendibleHTableDirectoryPage::GetLocalDepthMask(uint32_t bucket_idx) const -> uint32_t {
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+
+  return (1 << local_depths_[bucket_idx]) - 1;
+}
+
+auto ExtendibleHTableDirectoryPage::GetGlobalDepth() const -> uint32_t { 
+  return global_depth_;  
+}
+
+auto ExtendibleHTableDirectoryPage::GetMaxDepth() const -> uint32_t {
+  return max_depth_;
+}
 
 void ExtendibleHTableDirectoryPage::IncrGlobalDepth() {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  BUSTUB_ASSERT(global_depth_ < max_depth_, "global depth cannot be greater than max depth");
+
+  global_depth_++;
 }
 
 void ExtendibleHTableDirectoryPage::DecrGlobalDepth() {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  global_depth_--;
+
+  BUSTUB_ASSERT(global_depth_ > 0, "global depth cannot be less than 0");
 }
 
-auto ExtendibleHTableDirectoryPage::CanShrink() -> bool { return false; }
+auto ExtendibleHTableDirectoryPage::CanShrink() -> bool { 
+  for (uint32_t i = 0; i < Size(); ++i) {
+    if (local_depths_[i] == global_depth_) {
+      return false;
+    }
+  }
+  return true;
+}
 
-auto ExtendibleHTableDirectoryPage::Size() const -> uint32_t { return 0; }
+auto ExtendibleHTableDirectoryPage::Size() const -> uint32_t { 
+  return 1 << global_depth_;
+}
 
-auto ExtendibleHTableDirectoryPage::GetLocalDepth(uint32_t bucket_idx) const -> uint32_t { return 0; }
+auto ExtendibleHTableDirectoryPage::MaxSize() const -> uint32_t {
+  return HTABLE_DIRECTORY_ARRAY_SIZE;
+}
+
+auto ExtendibleHTableDirectoryPage::GetLocalDepth(uint32_t bucket_idx) const -> uint32_t { 
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+
+  return local_depths_[bucket_idx];  
+}
 
 void ExtendibleHTableDirectoryPage::SetLocalDepth(uint32_t bucket_idx, uint8_t local_depth) {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+  
+  local_depths_[bucket_idx] = local_depth; 
 }
 
 void ExtendibleHTableDirectoryPage::IncrLocalDepth(uint32_t bucket_idx) {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+  BUSTUB_ASSERT(local_depths_[bucket_idx] + 1 == max_depth_, "Cant increment local depth for this bucket_idx");
+
+  local_depths_[bucket_idx]++;
 }
 
 void ExtendibleHTableDirectoryPage::DecrLocalDepth(uint32_t bucket_idx) {
-  throw NotImplementedException("ExtendibleHTableDirectoryPage is not implemented");
-}
+  BUSTUB_ASSERT(bucket_idx < Size(), "Invalid bucket_idx");
+  BUSTUB_ASSERT(local_depths_[bucket_idx] - 1 == -1, "Cant decrement local depth for this bucket_idx");
+
+  local_depths_[bucket_idx]--;
 
 }  // namespace bustub
