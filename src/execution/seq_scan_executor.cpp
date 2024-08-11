22960db1062158e9a14fdac361032755da972d46
jishrup <jishnusm1997@gmail.com>
2024-08-10 18:29:03 -0700

    Implement SeqScanExecutor: Add sequential scan functionality
    - Completed SeqScanExecutor initialization to set up table iteration.
    - Implemented Next method to iterate over table tuples and return valid (non-deleted) tuples.
    - Integrated with TableHeap and catalog for accurate table scanning.

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,39 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx),  table_info_(nullptr) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() { 
+  // Get the table information from the catalog
+  table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  // Initialize the unique_ptr with a new TableIterator
+  table_iter_ = std::make_unique<TableIterator>(table_info_->table_->MakeIterator());
+
+}
+
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+  // Iterate through the table to find the next valid tuple
+  while (!table_iter_->IsEnd()) {
+    // Get the current tuple
+    auto tup = table_iter_->GetTuple();
+    *tuple = std::move(tup.second);
+    *rid = tuple->GetRid();
+
+    // Get the meta data of the tuple
+    auto meta = std::move(tup.first);
+
+    // Move to the next tuple in the table
+    ++(*table_iter_);
+
+    // Check if the tuple is deleted
+    if (!meta.is_deleted_) {
+      tuple = NULL;
+      rid = NULL;
+      return true;
+    }
+  }
+  return false;  
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -50,5 +50,9 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+
+  /** variables containing table information and table iterator */
+  TableInfo *table_info_;
+  std::unique_ptr<TableIterator> table_iter_;
 };
 }  // namespace bustub
